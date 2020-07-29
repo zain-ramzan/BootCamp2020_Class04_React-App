be15f0331d702f317dd198237de76a4f5f7fd094
ZainRamzan <64620737+ZainRamzanGorsi@users.noreply.github.com>
2020-07-29 17:50:17 +0500

    Update the Web Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [Mode, setMode] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${Mode ? 'Daylight' : 'Nightlight'}`}>
+      <h1>Assalm-u-Aliakum ! My name is <b>Zain Ramzan Gorsi</b></h1>
+      <h3>Class No # 04 | React Base App | BootCamp2020-PanaCloud | 29/July/2020</h3>
+      <hr/>
+      <h2>Mode : {Mode ? 'Daylight' : 'Nightlight'}</h2>
+      <button className="btn" onClick={()=> setMode(!Mode)}>Change the Mode</button>
+      <hr/>
+      <h2>Count the Counting : {count}</h2>
+      <button className="btn" onClick={()=> setCount(++count)}>Start the Counter</button>
+      <button className="btn" onClick={() => window.location.reload(false)}>Click to Refresh!</button>
+      <hr/>
     </div>
   );
 }
